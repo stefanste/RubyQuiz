13981c8e699745e9a24ade65c58683837d24db31
Stefan <stefanste@gmail.com>
2011-11-24 09:35:36 +1030

    Number to Roman Numeral completed

--- rubyExercise8.rb
+++ rubyExercise8.rb
@@ -1,26 +1,19 @@
 def convert(input, type)
   case(type)
-  when 'roman'
+  when 'roman'    # Roman numeral to Number
     theNumber = 0
     i = 0
     while i < input.length do
       cl = input[i]
       nl = input[i+1]
       amountToAdd = 0
-      if (cl == 'I' && nl == 'V')
-        amountToAdd += 4
-      elsif (cl == 'I' && nl == 'X')
-        amountToAdd += 9
-      elsif (cl == 'X' && nl == 'L')
-        amountToAdd += 40
-      elsif (cl == 'X' && nl == 'C')
-        amountToAdd += 90
-      elsif (cl == 'C' && nl == 'D')
-        amountToAdd += 400
-      elsif (cl == 'C' && nl == 'M')
-        amountToAdd += 900
-      else
-        amountToAdd = 0
+      if (cl == 'I' && nl == 'V') then amountToAdd += 4
+      elsif (cl == 'I' && nl == 'X') then amountToAdd += 9
+      elsif (cl == 'X' && nl == 'L') then amountToAdd += 40
+      elsif (cl == 'X' && nl == 'C') then amountToAdd += 90
+      elsif (cl == 'C' && nl == 'D') then amountToAdd += 400
+      elsif (cl == 'C' && nl == 'M') then amountToAdd += 900
+      else amountToAdd = 0
       end
       
       if amountToAdd == 0
@@ -43,34 +36,51 @@ def convert(input, type)
       end
     end
     theNumber
-  when 'number'
+    
+  when 'number'     # Number to Roman Numeral
     theNumeral = ""
     currentDigit = input.length - 1
+    loopNumber = 0
     currentLetter = 'I'
     nextLetter = 'V'
     superNextLetter = 'X'
     while (currentDigit >= 0)
       currentDigitValue = input.to_s[currentDigit]
       if currentDigitValue.to_i <= 3 && currentDigitValue.to_i != 0
-        currentDigitValue.to_i.times do |i|
-          theNumeral << currentLetter
+        if loopNumber == 3
+          currentDigitValue.to_i.times do |i|
+            theNumeral = 'M' + theNumeral
+          end
+        else
+          currentDigitValue.to_i.times do |i|
+          theNumeral = currentLetter + theNumeral
+          end
         end
       elsif currentDigitValue.to_i == 4
-        theNumeral << currentLetter << nextLetter
+        theNumeral = (currentLetter << nextLetter) + theNumeral
       elsif currentDigitValue.to_i == 5
-        theNumeral << nextLetter
+        theNumeral = nextLetter << theNumeral
       elsif currentDigitValue.to_i >= 6 && currentDigitValue.to_i <= 8
         (currentDigitValue.to_i - 5).times do |i|
-          theNumeral << currentLetter
+          theNumeral = currentLetter + theNumeral
         end
         theNumeral = nextLetter + theNumeral
       elsif currentDigitValue.to_i == 9
-        theNumeral << currentLetter << superNextLetter
+        theNumeral = (currentLetter << superNextLetter) + theNumeral
       else  # zero
-        # Carry over somehow...
+        # 
       end
       currentDigit -= 1
-      currentLetter = 'X',nextLetter = 'L',superNextLetter = 'C'
+      loopNumber += 1
+      if loopNumber == 1
+        currentLetter = 'X'
+        nextLetter = 'L'
+        superNextLetter = 'C'
+      elsif loopNumber == 2
+        currentLetter = 'C'
+        nextLetter = 'D'
+        superNextLetter = 'M'
+      end
     end
     theNumeral
   else puts "Problems"
@@ -91,11 +101,11 @@ if input =~ /^[IVXLCDM]+$/  # Valid Roman numeral.
   puts(convert(input,'roman'))
 elsif input =~ /^[\d]+$/  # Valid number.
   puts(convert(input,'number'))
-else
-  
+else  # Filename
+  while (input != nil)
+    puts(convert(input,'number'))
+    input = gets().chomp
+  end
 end
-
-  
-# Write to stout...
   
 end
